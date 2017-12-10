3f9176041cd29a007df414b29bdb9c08274c8db3
MOZGIII <mike-n@narod.ru>
2017-12-10 15:18:54 +0300

    Added Client.Resolve at youtube package

---
+++ internal/youtube/resolve.go
@@ -0,0 +1,44 @@
+package youtube
+
+import (
+	"fmt"
+	"net/url"
+)
+
+// Resolve parses input and converts it to a YouTube
+// video URL.
+func (c *Client) Resolve(input string) (string, error) {
+	// First try to parse a video URL. If succeeded,
+	// return Video ID from the URL.
+	if videoURL, err := parseYouTubeVideoURL(input); err == nil {
+		return videoURL.VideoID()
+	}
+	// It's not a URL.
+	// Try to search YouTube.
+	result, err := c.Search(input)
+	if err != nil {
+		return "", err
+	}
+	return result.VideoID(), nil
+}
+
+func parseYouTubeVideoURL(input string) (*youTubeVideoURL, error) {
+	u, err := url.Parse(input)
+	if err != nil {
+		return nil, err
+	}
+	return &youTubeVideoURL{url: u}, nil
+}
+
+type youTubeVideoURL struct {
+	url *url.URL
+}
+
+func (u *youTubeVideoURL) VideoID() (string, error) {
+	values := u.url.Query()
+	v, ok := values["v"]
+	if !ok || len(v) == 0 {
+		return "", fmt.Errorf("no v param found in URL")
+	}
+	return v[0], nil
+}
